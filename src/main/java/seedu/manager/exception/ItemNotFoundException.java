2ec9c8868cdb37aa04fc7bac8979df1b43b463db
jemehgoh <jeremygoheh@gmail.com>
2024-10-14 14:38:24 +0800

    Change indentation to comply with coding standard

--- src/main/java/seedu/manager/exception/ItemNotFoundException.java
+++ src/main/java/seedu/manager/exception/ItemNotFoundException.java
@@ -4,10 +4,10 @@ package seedu.manager.exception;
  * Signals that the specified item was not found in a given list
  */
 public class ItemNotFoundException extends Exception {
-  /**
-   * @param message relevant information on the type of item that was not found
-   */
-  public ItemNotFoundException(String message) {
+    /**
+    * @param message relevant information on the type of item that was not found
+    */
+    public ItemNotFoundException(String message) {
         super(message);
     }
 }
