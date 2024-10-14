a6e20a1fd769df1fc489fb499d0d35e06f40cefe
jemehgoh <jeremygoheh@gmail.com>
2024-10-14 09:11:36 +0800

    Add an ItemNotFoundException to handle missing item errors

---
+++ src/main/java/seedu/manager/exception/ItemNotFoundException.java
@@ -0,0 +1,13 @@
+package seedu.manager.exception;
+
+/**
+ * Signals that the specified item was not found in a given list
+ */
+public class ItemNotFoundException extends Exception {
+  /**
+   * @param message relevant information on the type of item that was not found
+   */
+  public ItemNotFoundException(String message) {
+        super(message);
+    }
+}
