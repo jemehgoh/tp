5516bd1b6c531fd1612597facd92904f24767d7f
jemehgoh <jeremygoheh@gmail.com>
2024-10-14 16:04:46 +0800

    Add unit tests and fix io redirection tests to fit new functionality

--- src/test/java/seedu/manager/command/RemoveCommandTest.java
+++ src/test/java/seedu/manager/command/RemoveCommandTest.java
@@ -5,6 +5,7 @@ import seedu.manager.event.EventList;
 import seedu.manager.exception.ItemNotFoundException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RemoveCommandTest {
 
@@ -21,14 +22,31 @@ public class RemoveCommandTest {
     }
 
     @Test
-    public void remove_oneParticipantWrongly_success() throws ItemNotFoundException {
+    public void remove_oneParticipantWrongly_throwException() throws ItemNotFoundException {
         EventList eventList = new EventList();
 
         eventList.addEvent("Event 1", "2024-10-10 10:00", "Venue A");
         eventList.addParticipantToEvent("Tom", "Event 1");
         eventList.addParticipantToEvent("Harry", "Event 1");
-        eventList.removeParticipantFromEvent("Tom", "Event 2");
 
-        assertEquals(2, eventList.getEvent(0).getParticipantCount());
+        assertThrows(ItemNotFoundException.class, () -> {eventList.removeParticipantFromEvent("Tom", "Event 2");});
+    }
+
+    @Test
+    public void remove_oneEvent_success() throws ItemNotFoundException {
+        EventList eventList = new EventList();
+
+        eventList.addEvent("Event 1", "2024-10-10 10:00", "Venue A");
+        eventList.addEvent("Event 2", "2024-10-31 21:00", "Venue B");
+        eventList.removeEvent("Event 2");
+
+        assertEquals(eventList.getListSize(), 1);
+    }
+
+    @Test
+    public void remove_invalidEvent_throwsException() throws ItemNotFoundException {
+        EventList eventList = new EventList();
+
+        assertThrows(ItemNotFoundException.class, () -> {eventList.removeEvent("Event 2");});
     }
 }
